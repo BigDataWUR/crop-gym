24e41a4f3c2e3020ef3a92b9e03521a07878d3a2
Hiske Overweg <hiske.overweg@wur.nl>
2021-01-08 15:29:02 +0100

    send management signals directly instead of via agromanagement file

--- gym-crop/gym_crop/envs/irrigation_env.py
+++ gym-crop/gym_crop/envs/irrigation_env.py
@@ -1,19 +1,11 @@
 import datetime
 import gym
-from gym import error, spaces, utils
-from gym.utils import seeding
 import os
 import yaml
 import copy
-
 import numpy as np
 import pandas as pd
-
-from pcse.fileinput import ExcelWeatherDataProvider
-from pcse.models import Wofost71_WLP_FD
-from pcse.fileinput import CABOFileReader, YAMLCropDataProvider
-from pcse.util import WOFOST71SiteDataProvider
-from pcse.base import ParameterProvider
+import pcse
 
 data_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'env_data/')
 
@@ -21,18 +13,18 @@ class IrrigationEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
     def __init__(self):
-        self.action_space = spaces.Discrete(2)
-        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(74,))
-        self.past_actions = []
-        crop = YAMLCropDataProvider()
-        soil = CABOFileReader(os.path.join(data_dir, "soil", "ec3.soil"))
-        site = WOFOST71SiteDataProvider(WAV=100,CO2=360)
-        self.parameterprovider = ParameterProvider(soildata=soil, cropdata=crop, sitedata=site)
+        self.action_space = gym.spaces.Discrete(2)
+        self.observation_space = gym.spaces.Box(low=-np.inf, high=np.inf, shape=(74,))
+        crop = pcse.fileinput.YAMLCropDataProvider()
+        soil = pcse.fileinput.CABOFileReader(os.path.join(data_dir, "soil", "ec3.soil"))
+        site = pcse.util.WOFOST71SiteDataProvider(WAV=100,CO2=360)
+        self.parameterprovider = pcse.base.ParameterProvider(soildata=soil, cropdata=crop, sitedata=site)
         weatherfile = os.path.join(data_dir, 'meteo', 'nl1.xlsx')
-        self.weatherdataprovider = ExcelWeatherDataProvider(weatherfile)
-        self.agromanagement, self.crop_start_date, self.crop_end_date = self._load_agromanagement_data()
-        self.dates = [self.crop_start_date]
+        self.weatherdataprovider = pcse.fileinput.ExcelWeatherDataProvider(weatherfile)
+        self.agromanagement, self.crop_end_date = self._load_agromanagement_data()
+        self.wofost = pcse.models.Wofost71_WLP_FD(self.parameterprovider, self.weatherdataprovider, self.agromanagement)
         self.intervention_interval = 7
+        self.weather_forecast_length = 7
 
     def step(self, action):
         """
@@ -62,64 +54,50 @@ class IrrigationEnv(gym.Env):
                  However, official evaluations of your agent are not allowed to
                  use this for learning.
         """
-        self.past_actions.append(action)
-        # observation takes place one week after the chosen action
-        self.dates.append(self.dates[-1] + datetime.timedelta(self.intervention_interval))
-        agromanagement = self._create_agromanagement_file()
-        wofost = Wofost71_WLP_FD(self.parameterprovider, self.weatherdataprovider, agromanagement)
-        wofost.run_till_terminate()
-        output = pd.DataFrame(wofost.get_output()).set_index("day")
-        crop_observation = np.array(output.iloc[-1])
-        # forecast for the week after the observation
-        weather_forecast = get_weather(self.weatherdataprovider, self.dates[-1], 7)
-        observation = np.concatenate([crop_observation, weather_forecast.flatten()])
+        self._take_action(action)
+        output = self._run_simulation()
+        observation = self._process_output(output)
 
         growth = output['TWSO'][-1] - output['TWSO'][-1-self.intervention_interval]
         reward = growth if not np.isnan(growth) else 0
 
-        done = self.dates[-1] > self.crop_end_date
+        done = self.date > self.crop_end_date
 
-        return observation, reward, done, {'management':yaml.dump(agromanagement)}
-
-    def _create_agromanagement_file(self, set_campaign_end=None):
-        event_table = []
-        for date, action in zip(self.dates[:-1], self.past_actions):
-            if action == 1:
-                event_table.append({date: {'amount': 10, 'efficiency': 0.7}})
-        new_management = copy.deepcopy(self.agromanagement)
-        next(iter(new_management[0].values()))['TimedEvents'][0]['events_table'] = event_table
-        if set_campaign_end:
-            new_management.append({set_campaign_end: None})
-        else:
-            # terminate on the observation date
-            new_management.append({self.dates[-1]: None})
-
-        return new_management
+        return observation, reward, done, {}
 
     def _load_agromanagement_data(self):
         with open(os.path.join(data_dir, 'agro/agromanagement_irrigation.yaml')) as file:
             agromanagement = yaml.load(file, Loader=yaml.SafeLoader)
-        crop_start_date = list(agromanagement[0].values())[0]['CropCalendar']['crop_start_date']
         crop_end_date = list(agromanagement[0].values())[0]['CropCalendar']['crop_end_date']
-        return agromanagement, crop_start_date, crop_end_date
+        return agromanagement, crop_end_date
+
+    def _process_output(self, output): 
+        crop_observation = np.array(output.iloc[-1])
+        # forecast for the week after the observation
+        weather_forecast = get_weather(self.weatherdataprovider, self.date, self.weather_forecast_length)
+        observation = np.concatenate([crop_observation, weather_forecast.flatten()])
+        observation = np.nan_to_num(observation)
+        return observation
+
+    def _run_simulation(self):
+        self.wofost.run(days=self.intervention_interval)
+        output = pd.DataFrame(self.wofost.get_output()).set_index("day")
+        output = output.fillna(value=np.nan)
+        self.date = output.index[-1]
+        return output
+
+    def _take_action(self, action):
+        if action==1:
+            self.wofost._send_signal(signal=pcse.signals.irrigate, amount=10, efficiency=0.7) # water in cm
 
     def seed(self, seed=None):
         return
 
     def reset(self):
-        self.past_actions = []
-        self.dates = [self.crop_start_date]
-        agromanagement = self._create_agromanagement_file(set_campaign_end=self.crop_start_date+datetime.timedelta(1))
-        wofost = Wofost71_WLP_FD(self.parameterprovider, self.weatherdataprovider, agromanagement)
-        wofost.run_till_terminate()
-        output = pd.DataFrame(wofost.get_output()).set_index("day")
-        crop_observation = np.array(output.iloc[-2])
-        # forecast for the week after the observation
-        weather_forecast = get_weather(self.weatherdataprovider, self.dates[-1], 7)
-        observation = np.concatenate([crop_observation, weather_forecast.flatten()])
-
+        self.wofost = pcse.models.Wofost71_WLP_FD(self.parameterprovider, self.weatherdataprovider, self.agromanagement)
+        output = self._run_simulation()
+        observation = self._process_output(output)
         return observation
-
 
     def render(self, mode='human', close=False):
         pass
